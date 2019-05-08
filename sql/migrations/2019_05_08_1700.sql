9454326766c266c1daebabcc5abafa08b52e208f
NomisCZ <nomiscz@outlook.com>
2019-05-08 18:00:21 +0200

    #18 #16 Add missing CS:GO weapons
    Add missing knives (https://counterstrike.fandom.com/wiki/Knife)

---
+++ sql/migrations/2019_05_08_1700.sql
@@ -0,0 +1,21 @@
+/*
+:? Reason: CS:GO - add M4A1-S and missing knives
+:i Info: https://github.com/NomisCZ/hlstatsx-community-edition/issues/18
+:! Change: Insert new rows
+*/
+INSERT INTO `hlstats_Weapons` (`game`, `code`, `name`, `modifier`) VALUES
+('csgo', 'm4a1_silencer', 'M4A1 Silencer', 1.00),
+('csgo', 'bayonet', 'Bayonet', 2.00),
+('csgo', 'knife_butterfly', 'Butterfly Knife', 2.00),
+('csgo', 'knife_falchion', 'Falchion Knife', 2.00),
+('csgo', 'knife_flip', 'Flip Knife', 2.00),
+('csgo', 'knife_gut', 'Gut Knife', 2.00),
+('csgo', 'knife_tactical', 'Huntsman Knife', 2.00),
+('csgo', 'knife_karambit', 'Karambit', 2.00),
+('csgo', 'knife_m9_bayonet', 'M9 Bayonet', 2.00),
+('csgo', 'knife_push', 'Shadow Daggers', 2.00),
+('csgo', 'knife_survival_bowie', 'Bowie Knife', 2.00),
+('csgo', 'knife_ursus', 'Ursus Knife', 2.00),
+('csgo', 'knife_gypsy_jackknife', 'Navaja Knife', 2.00),
+('csgo', 'knife_stiletto', 'Stiletto Knife', 2.00),
+('csgo', 'knife_widowmaker', 'Talon Knife', 2.00);
