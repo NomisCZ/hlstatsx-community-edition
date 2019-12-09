f7b836656bbd7415b41afbc25046ca2d431310bf
NomisCZ <NomisCZ@users.noreply.github.com>
2019-12-09 21:27:05 +0100

    #40 Add missing CS:GO knives (#41)
    Update install.sql
    Add 2019_12_09_2100.sql migration
    Update images - rotation

---
+++ sql/migrations/2019_12_09_2100.sql
@@ -0,0 +1,11 @@
+/*
+:? Reason: CS:GO - add missing knives, Operation Shattered Web
+:i Info: https://github.com/NomisCZ/hlstatsx-community-edition/issues/40
+:! Change: Insert new rows
+*/
+INSERT INTO `hlstats_Weapons` (`game`, `code`, `name`, `modifier`) VALUES
+('csgo', 'knife_outdoor', 'Nomad Knife', 2.00),
+('csgo', 'knife_skeleton', 'Skeleton Knife', 2.00),
+('csgo', 'knife_canis', 'Survival Knife', 2.00),
+('csgo', 'knife_cord', 'Paracord Knife', 2.00),
+('csgo', 'knife_css', 'Classic Knife', 2.00);
